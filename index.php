728becadcf16b79b28d12b92506b908edea5904b
AkerKratz <89996536+AkerKratz@users.noreply.github.com>
2023-06-30 11:10:15 +0300

    1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>laba 8</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<a href="./zav1.php" target="_blank">task1</a>
+	<a href="./zav2.php" target="_blank">task2</a>
+	<a href="./zav3.php" target="_blank">task3</a>
+	<a href="./index.html" target="_blank">task4</a>
+</body>
+</html>
